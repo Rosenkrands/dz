6f80b273755e9a3bed6a95b3d0091e844ae83355
Kasper Rosenkrands <krosen17@student.aau.dk>
2022-03-10 12:14:12 +0100

    update to greedy clustering approach

--- R/clustering.R
+++ R/clustering.R
@@ -9,37 +9,94 @@
 #' @return A list ...
 #' @export
 #'
-clustering <- function(inst, k, cluster_method = "pam") {
+clustering <- function(inst, k, cluster_method = c("greedy_p")) {
   # For testing purposes:
-  # inst = test_instances$p7_chao; k = 4; cluster_method = "pam"
+  # inst = test_instances$p7_chao; k = 4; cluster_method = "greedy_p"
 
   # Save only the intermediate points for clustering
   in_points <- inst$points |> dplyr::filter(point_type == "intermediate")
 
-  if (cluster_method == "pam") {
-    # Perform PAM clustering on the intermediate points and save the clustering vector
-    assign <- (cluster::pam(
-      x = in_points |> dplyr::select(x, y),
-      k = k,
-      metric = "euclidean"
-    ))$clustering
-  } else {
-    stop(paste0("cluster_method ", cluster_method, " not implemented"))
+  greedy_clustering <- function() {
+    # Compute edges in delaunay triangulation
+    tri <- (deldir::deldir(inst$points$x, inst$points$y))$delsgs
+
+    # add the source node to each zone
+    zones <- list()
+    for (i in 1:k) {
+      zones[[i]] <- c(1)
+    }
+
+    # calculate distance matrix
+    dst <- Rfast::Dist(
+      inst$points |> dplyr::select(x,y),
+      method = "euclidean"
+    )
+
+    # keep track of unassigned points
+    unassigned <- in_points
+    while(nrow(unassigned) > 0) {
+      cat("unassigned points: ", nrow(unassigned), "\n")
+      for (i in 1:k) {
+        cat("zone: ", i, "\n")
+        # assign nearest point to each zone
+        points_in_zone <- zones[[i]]
+
+        # Find the delaunay neighbors
+        nghbr <- tri |>
+          dplyr::filter((ind1 %in% unassigned$id) | (ind2 %in% unassigned$id)) |>
+          dplyr::filter(
+            ((ind1 %in% points_in_zone) & !(ind2 %in% points_in_zone)) |
+            ((ind2 %in% points_in_zone) & !(ind1 %in% points_in_zone))
+          ) |>
+          dplyr::select(ind1, ind2) |>
+          tidyr::pivot_longer(cols = c(ind1, ind2), names_to = NULL, values_to = "id") |>
+          dplyr::filter(!(id %in% points_in_zone)) |>
+          dplyr::pull(id)
+
+        # if there are no neighbors go to next iteration
+        if (length(nghbr) < 1) next
+
+        # find the closest point
+        closest_points <- expand.grid(id1 = points_in_zone, id2 = nghbr) |>
+          dplyr::rowwise() |>
+          dplyr::mutate(dist = dst[id1, id2]) |>
+          dplyr::ungroup() |>
+          dplyr::slice_min(dist, n = 1) |>
+          dplyr::pull(id2)
+
+        # add to zone
+        zones[[i]] <- append(zones[[i]], closest_points)
+
+        # update unassigned
+        unassigned <- unassigned |> dplyr::filter(!(id %in% closest_points))
+      }
+    }
+
+    # add the sink node to each zone
+    for (i in 1:k) {
+      zones[[i]] <- append(zones[[i]], nrow(inst$points))
+    }
+
+    return(
+      inst$points |>
+        dplyr::left_join(
+          tibble::tibble(zone = 1:k, id = zones) |>
+            tidyr::unnest(cols = id),
+          by = c("id" = "id")
+        )
+    )
   }
 
-  # Add the clustering vector to the intermediate points
-  in_points$zone <- assign
-
-  # Join zone assignment on all points
-  inst$points <- inst$points |>
-    dplyr::left_join(in_points |> dplyr::select(id, zone), by = c("id" = "id"))
+  inst$points <- switch(
+    cluster_method,
+    "greedy_p" = greedy_clustering()
+  )
 
   structure(
     list(
       "instance" = inst,
       "k" = k,
-      "cluster_method" = cluster_method,
-      "in_points" = in_points
+      "cluster_method" = cluster_method
     ),
     class = "clustering"
   )
@@ -55,17 +112,20 @@ clustering <- function(inst, k, cluster_method = "pam") {
 #' @export
 #'
 plot.clustering <- function(clust) {
+  # For testing purposes:
+  # clust <- clustering(inst = test_instances$p7_chao, k = 4, cluster_method = "greedy_p")
+
   ggplot2::ggplot() +
     # Find the smallest polygon including all nodes in each zone
     ggalt::geom_encircle(
-      data = clust$in_points,
+      data = clust$instance$points,
       ggplot2::aes(x = x, y = y, group = zone, fill = as.character(zone)),
       s_shape = 1, expand = 0, alpha = 0.2
     ) +
     # Plot the intermediate node with color according to score
     ggplot2::geom_point(
       data = clust$instance$points |> dplyr::filter(point_type == "intermediate"),
-      ggplot2::aes(x, y, color = score, shape = point_type)
+      ggplot2::aes(x, y, color = as.character(zone), shape = point_type)
     ) +
     # Plot the terminal nodes
     ggplot2::geom_point(
