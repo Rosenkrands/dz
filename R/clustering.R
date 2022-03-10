405604b199a5e0ad9e112f5a3cdb0ca56d5dfa95
Kasper Rosenkrands <krosen17@student.aau.dk>
2022-03-10 15:26:37 +0100

    work on the local search procedure for clustering

--- R/clustering.R
+++ R/clustering.R
@@ -9,28 +9,28 @@
 #' @return A list ...
 #' @export
 #'
-clustering <- function(inst, k, cluster_method = c("greedy_p")) {
+clustering <- function(inst, k, cluster_method = c("greedy", "local_search")) {
   # For testing purposes:
-  # inst = test_instances$p7_chao; k = 4; cluster_method = "greedy_p"
+  # inst = test_instances$p7_chao; k = 4; cluster_method = "greedy"
 
   # Save only the intermediate points for clustering
   in_points <- inst$points |> dplyr::filter(point_type == "intermediate")
 
-  greedy_clustering <- function() {
-    # Compute edges in delaunay triangulation
-    tri <- (deldir::deldir(inst$points$x, inst$points$y))$delsgs
+  # Compute edges in delaunay triangulation
+  tri <- (deldir::deldir(inst$points$x, inst$points$y))$delsgs
 
+  # calculate distance matrix
+  dst <- Rfast::Dist(
+    inst$points |> dplyr::select(x,y),
+    method = "euclidean"
+  )
+
+  greedy_clustering <- function() {
     # add the source node to each zone
     zones <- list()
     for (i in 1:k) {
       zones[[i]] <- c(1)
     }
-
-    # calculate distance matrix
-    dst <- Rfast::Dist(
-      inst$points |> dplyr::select(x,y),
-      method = "euclidean"
-    )
 
     # keep track of unassigned points
     unassigned <- in_points
@@ -51,6 +51,7 @@ clustering <- function(inst, k, cluster_method = c("greedy_p")) {
           dplyr::select(ind1, ind2) |>
           tidyr::pivot_longer(cols = c(ind1, ind2), names_to = NULL, values_to = "id") |>
           dplyr::filter(!(id %in% points_in_zone)) |>
+          dplyr::distinct() |>
           dplyr::pull(id)
 
         # if there are no neighbors go to next iteration
@@ -78,18 +79,89 @@ clustering <- function(inst, k, cluster_method = c("greedy_p")) {
     }
 
     return(
-      inst$points |>
-        dplyr::left_join(
-          tibble::tibble(zone = 1:k, id = zones) |>
-            tidyr::unnest(cols = id),
-          by = c("id" = "id")
-        )
+      list(
+        "inst_points" = inst$points |>
+          dplyr::left_join(
+            tibble::tibble(zone = 1:k, id = zones) |>
+              tidyr::unnest(cols = id),
+            by = c("id" = "id")
+          ),
+        "zones" = zones
+      )
+
     )
+  }
+
+  local_search_clustering <- function() {
+    # Compute the initial clustering
+    gclust <- greedy_clustering()
+
+    # storing the points and zones
+    inst$points <- gclust$inst_points
+    zones <- gclust$zones
+
+    cluster_eval <- function(zone) {
+      # For testing purposes:
+      # zone = c(1, 40, 42, 76, 22, 60, 14, 75, 100, 61, 12, 98, 24, 37, 23, 13, 63, 27, 91, 49, 85, 78, 67, 79, 36, 102)
+
+      total_profit <- inst$points |>
+        dplyr::filter(id %in% zone) |>
+        dplyr::summarise(score = sum(score)) |>
+        dplyr::pull(score)
+
+      average_distance <- expand.grid(id1 = zone, id2 = zone) |>
+        dplyr::rowwise() |>
+        dplyr::mutate(dist = dst[id1, id2]) |>
+        dplyr::ungroup() |>
+        dplyr::filter(dist != 0) |>
+        dplyr::summarise(dist = mean(dist)) |>
+        dplyr::pull(dist)
+
+      average_distance/total_profit
+    }
+
+    # define the operators for the local search
+
+    # take a point from one cluster and add it to another
+    insertion <- function() {
+      #do.call(sum, lapply(zones, cluster_eval))
+
+      # A candidate should be understood as moving the respective id to the respective zone
+      candidates <- tri |>
+        dplyr::select(ind1, ind2) |>
+        dplyr::filter(
+          ind1 != 1, ind2 != 1, ind1 != nrow(inst$points), ind2 != nrow(inst$points)
+        ) |>
+        dplyr::inner_join(inst$points |> dplyr::select(id, zone), by = c("ind1" = "id")) |>
+        dplyr::inner_join(inst$points |> dplyr::select(id, zone), by = c("ind2" = "id")) |>
+        dplyr::filter(zone.x != zone.y) |>
+        tidyr::pivot_longer(cols = c(zone.x, zone.y), values_to = "zone_id") |>
+        dplyr::mutate(id1 = ifelse(name == "zone.y", ind1, ind2),
+                      id2 = ifelse(name == "zone.x", ind1, ind2)) |>
+        dplyr::select(id = id1, zone_id)
+
+      insert_eval <- function(id, zone_id) {
+        # find where the point is coming from and remove it
+        for (i in 1:k) {
+          if (i == zone_id) next
+          if (id %in% zones[[i]]) {
+            zones[[i]] <- zones[[i]][zones[[i]] != id]
+          }
+        }
+
+        # then insert it in the new zone
+        zones[[zone_id]] <- append(
+          zones[[zone_id]],
+          id,
+          after = length(zones[[zone_id]]) - 1
+        )
+      }
+    }
   }
 
   inst$points <- switch(
     cluster_method,
-    "greedy_p" = greedy_clustering()
+    "greedy" = (greedy_clustering())$inst_points
   )
 
   structure(
@@ -111,17 +183,34 @@ clustering <- function(inst, k, cluster_method = c("greedy_p")) {
 #' @return A ggplot object
 #' @export
 #'
-plot.clustering <- function(clust) {
+plot.clustering <- function(clust, delaunay = T) {
   # For testing purposes:
-  # clust <- clustering(inst = test_instances$p7_chao, k = 4, cluster_method = "greedy_p")
+  # clust <- clustering(inst = test_instances$p7_chao, k = 4, cluster_method = "greedy")
 
-  ggplot2::ggplot() +
+  p <- ggplot2::ggplot()
+
+    # If either delaunay or voronoi is true we compute the triangulation
+  if (delaunay) {
+    tri <- deldir::deldir(clust$instance$points$x, clust$instance$points$y)
+  }
+
+  # Add delaunay edges
+  if (delaunay) {
+    p <- p +
+      ggplot2::geom_segment(
+        data = tri$delsgs,
+        ggplot2::aes(x = x1, y = y1, xend = x2, yend = y2),
+        color = ggplot2::alpha("black", 0.3), linetype = "dashed"
+      )
+  }
+
+  p +
     # Find the smallest polygon including all nodes in each zone
-    ggalt::geom_encircle(
-      data = clust$instance$points,
-      ggplot2::aes(x = x, y = y, group = zone, fill = as.character(zone)),
-      s_shape = 1, expand = 0, alpha = 0.2
-    ) +
+    # ggalt::geom_encircle(
+    #   data = clust$instance$points,
+    #   ggplot2::aes(x = x, y = y, group = zone, fill = as.character(zone)),
+    #   s_shape = 1, expand = 0, alpha = 0.2
+    # ) +
     # Plot the intermediate node with color according to score
     ggplot2::geom_point(
       data = clust$instance$points |> dplyr::filter(point_type == "intermediate"),
