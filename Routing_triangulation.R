5d907b1f46cca4c8b582ebc200b8554b379cc29b
casper pedersen <cbpe17@student.aau.dk>
2022-03-17 11:45:48 +0100

    Comments

--- Routing_triangulation.R
+++ Routing_triangulation.R
@@ -101,6 +101,7 @@ solve_routing <- function(obj = 'SDR', L = 100, zone_id = 1){
     }
     if (dist(last_in_current, New_last, g = g) + dist(New_last, 1, g = g) - dist(last_in_current,  1, g = g) < L){
       route <- append(route, all_short_path[2:length(all_short_path)], after = length(route)-1)
+      # Construct route back to base
       all_short_path_return <- dist2(New_last, 1, g = g)
       # For-loop to remove all new distances, not just the last in new shortest path
       L <- L + dist(last_in_current, 1, g = g)
@@ -112,6 +113,7 @@ solve_routing <- function(obj = 'SDR', L = 100, zone_id = 1){
       }
       # print(route)
     } else {
+      # Add route back to base
       route <- append(route, all_short_path_return[2:(length(all_short_path_return)-1)], after = length(route)-1)
       # Switch last two before terminal
       # route <- replace(route, c(length(route)-1, length(route)-2), route[c(length(route)-2, length(route)-1)])
