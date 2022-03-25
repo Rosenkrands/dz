9ef9ab05266f5bc179279b81ff8db2af37ea5d6b
Kasper Rosenkrands <krosen17@student.aau.dk>
2022-03-25 19:59:40 +0100

    added additional data to the instance object

--- R/instance.R
+++ R/instance.R
@@ -9,6 +9,9 @@
 #' @return `instance` returns an object of \code{\link{class}} "`instance`".
 #'   An object of class "`instance`" is a list containing:
 #'   `points` a `tibble` containing coordinates and scores for each point.
+#'   `edges` a `tibble` containing the (weighted) edges in the graph.
+#'   `g` an igraph object of the graph making up the instance.
+#'   `dst` a matrix containing the length of shortest paths between points.
 #'   `n` an integer equal to the number of points in the instance.
 #' @export
 #'
@@ -17,7 +20,23 @@ instance <- function(path_to_file) {
     tibble::tibble() |>
     dplyr::rename(x = V1, y = V2, score = V3) |>
     dplyr::mutate(id = dplyr::row_number(), .before = dplyr::everything(),
-                  point_type = ifelse(score == 0, "terminal", "intermediate"))
+                  point_type = ifelse(score == 0, "terminal", "intermediate")) |>
+    dplyr::filter(dplyr::row_number() != dplyr::n()) # We assume source and sink are the node
+
+  # Compute edges in delaunay triangulation
+  tri <- (deldir::deldir(points$x, points$y))$delsgs
+
+  # construct the igraph object
+  tri$dist <- sqrt((tri$x1 - tri$x2)^2 + (tri$y1 - tri$y2)^2) # We assume euclidean distance
+
+  g <- igraph::graph.data.frame(
+    tri |> dplyr::select(ind1, ind2, weight = dist),
+    directed = FALSE,
+    vertices = points |> dplyr::select(id, score)
+  )
+
+  # calculate distance matrix (based on shortest path)
+  dst <- igraph::distances(g, algorithm = "dijkstra")
 
   n <- nrow(points)
 
@@ -26,44 +45,38 @@ instance <- function(path_to_file) {
     (function(x) stringr::str_split(x, "/")[[1]])() |>
     utils::tail(n = 2) |> rev() |> paste(collapse = "_")
 
-  structure(list("points" = points, "n" = n, "name" = name), class = "instance")
+  structure(
+    list(
+      "points" = points,
+      "edges" = tri,
+      "g" = g,
+      "dst" = dst,
+      "n" = n,
+      "name" = name
+    ),
+    class = "instance"
+  )
 }
 
 #' Plot method for instance object
 #'
 #' @param inst Object of class `instance`
 #' @param delaunay Show edges from a Delaunay triangulation
-#' @param voronoi Show tiles from a Voronoi tesselation
 #'
 #' @return A ggplot object
 #' @export
 #'
-plot.instance <- function(inst, delaunay = FALSE, voronoi = FALSE) {
+plot.instance <- function(inst, delaunay = FALSE) {
   # Instantiate the ggplot object
   p <- ggplot2::ggplot()
-
-  # If either delaunay or voronoi is true we compute the triangulation
-  if (delaunay | voronoi) {
-    tri <- deldir::deldir(inst$points$x, inst$points$y)
-  }
 
   # Add delaunay edges
   if (delaunay) {
     p <- p +
       ggplot2::geom_segment(
-        data = tri$delsgs,
+        data = inst$edges,
         ggplot2::aes(x = x1, y = y1, xend = x2, yend = y2),
         color = ggplot2::alpha("black", 0.3), linetype = "dashed"
-      )
-  }
-
-  # Add voronoi tiles
-  if (voronoi) {
-    p <- p +
-      ggvoronoi::stat_voronoi(
-        data = inst$points |> dplyr::distinct(x,y),
-        ggplot2::aes(x,y),
-        geom = "path", color = ggplot2::alpha("black", 0.5)
       )
   }
 
@@ -71,7 +84,7 @@ plot.instance <- function(inst, delaunay = FALSE, voronoi = FALSE) {
   p <- p +
     ggplot2::geom_point(
       data = inst$points |> dplyr::filter(point_type == "intermediate"),
-      ggplot2::aes(x, y, color = score, shape = point_type)
+      ggplot2::aes(x, y, size = score, color = score, shape = point_type)
     ) +
     ggplot2::geom_point(
       data = inst$points |> dplyr::filter(point_type == "terminal"),
@@ -79,7 +92,7 @@ plot.instance <- function(inst, delaunay = FALSE, voronoi = FALSE) {
     ) +
     ggplot2::ggtitle(paste0("Instance: ", inst$name)) +
     ggplot2::theme_bw() +
-    ggplot2::guides(shape = "none")
+    ggplot2::guides(shape = "none", size = "none")
 
   return(p)
 }
