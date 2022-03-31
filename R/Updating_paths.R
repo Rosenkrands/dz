6c4e6f3978d5c3a1f2298bae368388f0cd1d3f20
casper pedersen <cbpe17@student.aau.dk>
2022-03-31 14:46:03 +0200

    Now respects the length consiraint taken from the solution

--- R/Updating_paths.R
+++ R/Updating_paths.R
@@ -179,7 +179,6 @@ route <- remaining_route[1:2]
 print(plot_progress())
 L_remaining <- initial_route$L
 while(length(remaining_nodes) != 0){
-  invisible(readline(prompt="Press [enter] to continue"))
   # Keep track of changes
   last_remaining_route <- remaining_route
   # Node the UAV flew from
@@ -209,9 +208,13 @@ while(length(remaining_nodes) != 0){
       point <- unique(edges %>% filter(ind2 == node) %>% select(x1 = x2, y1 = y2))
     }
     dist_to_edge[l] <- distancePointSegment(px = point$x1, py <- point$y1, x1 = current_line$x1, x2 = current_line$x2, y1 = current_line$y1, y2 = current_line$y2)
-    if (dist_to_edge[l] < r){
-      # Nodes in zone within viewing distance
-      candidates <- append(candidates, node)
+    # Remove the candidates that would (for sure) make the route to long if they were added
+    if (2*dist_to_edge[l] < L_remaining){
+      # Add the nodes in the zone, that are within viewing distance
+      if (dist_to_edge[l] < r){
+        # Nodes in zone within viewing distance
+        candidates <- append(candidates, node)
+      }
     }
   }
   # Evaluate how good the next planned node to be visited is when using realized score
@@ -257,7 +260,9 @@ while(length(remaining_nodes) != 0){
   # Add and remove these from the route according to (shortest paths) SDR
   # We remove two and add at least two
   longer_than_original <- 0
-  if ((max(SDR_cand) > SDR_planned_realized) & !(New_point %in% remaining_route)){
+  # Check length constraint
+  L_required <- dist(id_next, New_point, g = g) + dist(New_point, remaining_nodes[2], g = g)
+  if ((max(SDR_cand) > SDR_planned_realized) & !(New_point %in% remaining_route) & (L_remaining > L_required)){
     # Remove the node that would originally be visited after id_next
     remaining_route <- remaining_route[remaining_route != remaining_nodes[1]]
     # Add new
@@ -277,6 +282,12 @@ while(length(remaining_nodes) != 0){
     cat("Added a node not in original route", "\n")
     print(New_point)
   } else {
+    if (L_remaining < L_required){
+      d2_home <- dist2(id_next, 1, g = g)
+      route <- append(route, d2_home[2:(length(d2_home)-1)])
+      route <- append(route, 1)
+      break
+    }
     if (is.na(remaining_route[3])) {route <- append(route, c(remaining_route[2], 1)); break}
     route <- append(route, remaining_route[3])
     # Update remaining_route by removing the ones already visited (excluding id_now and id_next for the next iteration)
