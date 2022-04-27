3b5cd689ad0784afc5847aeb36e70561031e8134
Kasper Rosenkrands <krosen17@student.aau.dk>
2022-04-27 12:23:59 +0200

    updates to the test script

--- test.R
+++ test.R
@@ -3,15 +3,16 @@ set.seed(10)
 
 # Setting parameters
 inst <- test_instances$p7_chao
-L <- 100
-k <- 3
+L <- 80
+k <- 4
 variances <- generate_variances(inst)
 info <- generate_information(inst)
 num_routes <- 100
+plot(inst)
 
 # Prepare instance
 p_inst <- prepare_instance(inst, variances, info)
-(p <- plot(p_inst))
+plot(p_inst)
 
 # Clustering
 rb_clust <- rb_clustering(p_inst, L, k, num_routes, info)
@@ -22,3 +23,4 @@ system.time(sr <- starting_routes(inst, zones, L))
 plot(sr, inst)
 
 ur <- update_routes(sr, L, variances, info)
+plot(ur, inst)
