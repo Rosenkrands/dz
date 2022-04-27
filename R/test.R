1695550da9e79372ece6e24c09c32e9ecb9b3b20
Kasper Rosenkrands <krosen17@student.aau.dk>
2022-04-27 11:18:21 +0200

    test script for routing experimentation

---
+++ R/test.R
@@ -0,0 +1,22 @@
+library(dz)
+set.seed(5)
+
+# Setting parameters
+inst <- test_instances$p7_chao
+L <- 100
+k <- 3
+variances <- generate_variances(inst)
+info <- generate_information(inst)
+num_routes <- 100
+
+# Prepare instance
+p_inst <- prepare_instance(inst, variances, info)
+(p <- plot(p_inst))
+
+# Clustering
+rb_clust <- rb_clustering(p_inst, L, k, num_routes, info)
+plot(rb_clust)
+
+zones <- rb_clust$zones
+system.time(sr <- starting_routes(inst, zones, L))
+plot(sr, inst)
